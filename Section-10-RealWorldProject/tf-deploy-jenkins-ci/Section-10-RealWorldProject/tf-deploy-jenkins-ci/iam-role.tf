7435bace0f46535f81cc5a459aef7e781898e702
Eric <eriktalla@gmail.com>
2021-02-18 10:16:29 -0600

    Updated my system ip my terraform key.

--- Section-10-RealWorldProject/tf-deploy-jenkins-ci/Section-10-RealWorldProject/tf-deploy-jenkins-ci/iam-role.tf
+++ Section-10-RealWorldProject/tf-deploy-jenkins-ci/Section-10-RealWorldProject/tf-deploy-jenkins-ci/iam-role.tf
@@ -27,5 +27,5 @@ EOF
 
 resource "aws_iam_instance_profile" "ec2_profile" {
   name = "ec2_profile"
-  role = "${aws_iam_role.ec2_role.name}"
+  role = aws_iam_role.ec2_role.name
 }
--- Section-10-RealWorldProject/tf-deploy-jenkins-ci/Section-10-RealWorldProject/tf-deploy-jenkins-ci/policy.tf
+++ Section-10-RealWorldProject/tf-deploy-jenkins-ci/Section-10-RealWorldProject/tf-deploy-jenkins-ci/policy.tf
@@ -34,6 +34,6 @@ EOF
 }
 
 resource "aws_iam_role_policy_attachment" "s3-pol-attach" {
-  role       = "${aws_iam_role.ec2_role.name}"
-  policy_arn = "${aws_iam_policy.s3_policy.arn}"
+  role       = aws_iam_role.ec2_role.name
+  policy_arn = aws_iam_policy.s3_policy.arn
 }
